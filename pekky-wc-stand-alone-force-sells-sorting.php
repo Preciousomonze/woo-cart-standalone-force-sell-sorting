c899fa11b4f6fab6bbca4522349350ffb5c7772b
Preciousomonze <crispusomonze@gmail.com>
2020-10-19 16:58:20 +0100

    Fix:indenting and linting

--- pekky-wc-stand-alone-force-sells-sorting.php
+++ pekky-wc-stand-alone-force-sells-sorting.php
@@ -3,6 +3,7 @@
  * WooCommerce Cart Stand-alone and Force Sells Custom Sorting.
  *
  * @package pekky-wc-stand-alone-force-sells-sorting
+ * @author Precious Omonzejele (CodeXplorer 🤾🏽‍♂️🥞🦜🤡)
  *
  * @wordpress-plugin
  * Plugin Name: WooCommerce Cart Stand-alone and Force Sells Custom Sorting
@@ -19,6 +20,7 @@
 class Pekky_WC_Stand_Alone_Force_Sells_Sorting {
 
 	// The grouped Products.
+
 	/**
 	 *  Stand Alone Products
 	 *
@@ -27,6 +29,7 @@ class Pekky_WC_Stand_Alone_Force_Sells_Sorting {
 	 * @var array
 	 */
 	protected static $wc_stand_alone_products = array( 25221, 29237, 29359 );
+
 	/**
 	 * Friendly products that can stay in the cart with other products.
 	 *
@@ -35,7 +38,9 @@ class Pekky_WC_Stand_Alone_Force_Sells_Sorting {
 	 *
 	 * @var array
 	 */
+
 	protected static $wc_other_products = array( 28745, 28746, 28747, 28748 );
+
 	/**
 	 * Products attached to the standalone products.
 	 *
@@ -45,6 +50,7 @@ class Pekky_WC_Stand_Alone_Force_Sells_Sorting {
 	 * @var array
 	 */
 	protected static $wc_force_sells_products = array( 25556, 29234, 29444 );
+
 	/**
 	 * Products attached to the standalone products.
 	 *
@@ -57,6 +63,7 @@ class Pekky_WC_Stand_Alone_Force_Sells_Sorting {
 		'29237' => array( 25556 ),
 		'25221' => array( 29234, 29444 ),
 	);
+
 	/**
 	 * Suggested upsell products to show based on cart amount
 	 * All prices are assumed in $dollars.
@@ -84,17 +91,22 @@ class Pekky_WC_Stand_Alone_Force_Sells_Sorting {
 			'upsell_note' => 'Join The ELITE 3% Group By Upgrading To The 3% Diabetes Program Today for $397 + $19.99/month. (Best Value!)',
 		),
 	);
+
 	/**
 	 * Boostrap and shoot.
 	 */
 	public static function init() {
 		// Manipulate when added to cart.
 		add_filter( 'woocommerce_add_to_cart_validation', array( __CLASS__, 'sort_product_add_to_cart_validation' ), 20, 3 );
+
 		// Manipulate when removed from cart.
 		add_action( 'woocommerce_remove_cart_item', array( __CLASS__, 'sort_product_removal_from_cart' ), 10, 2 );
+
 		// Show Upsell notice in cart.
 		add_action( 'woocommerce_before_cart', array( __CLASS__, 'show_upsell_notice' ) );
+
 	}
+
 	/**
 	 * Cart Item adding validation
 	 *
@@ -110,11 +122,15 @@ class Pekky_WC_Stand_Alone_Force_Sells_Sorting {
 		$stand_alone_products     = self::$wc_stand_alone_products;
 		$other_products           = self::$wc_other_products;
 		$force_sells_products_map = self::$wc_force_sells_products_map;
+
 		// Add only level 2 or level three program to the cart.
 		if ( in_array( $product_id, $stand_alone_products, true ) ) {
+
 			self::pekky_sort_stand_alone_products( $product_id, $stand_alone_products, $force_sells_products_map );
+
 		} elseif ( in_array( $product_id, $other_products, true ) ) {
 			// Allow other products to be in cart together.
+
 			// Make sure stand alone products aren't in the cart.
 			foreach ( $stand_alone_products as $p_id ) {
 				// Is this standalone product in the cart?
@@ -127,6 +143,8 @@ class Pekky_WC_Stand_Alone_Force_Sells_Sorting {
 		}
 		return $passed;
 	}
+
+
 	/**
 	 * Remove Necessary Products
 	 *
@@ -140,10 +158,13 @@ class Pekky_WC_Stand_Alone_Force_Sells_Sorting {
 		$stand_alone_products = self::$wc_stand_alone_products;
 		$other_products       = self::$wc_other_products;
 		$force_sells_products = self::$wc_force_sells_products;
+
 		$cart_contents = $cart->get_cart_contents();
 		$cart_item     = isset( $cart_contents[ $cart_item_key ] ) ? $cart_contents[ $cart_item_key ] : array();
+
 		if ( isset( $cart_item['product_id'] ) ) {
 			$product_id = $cart_item['product_id'];
+
 			if ( in_array( $product_id, $stand_alone_products, true )
 			|| in_array( $product_id, $force_sells_products, true ) ) {
 				// Product is in standalone list, or force_sells list, empty cart.
@@ -151,6 +172,7 @@ class Pekky_WC_Stand_Alone_Force_Sells_Sorting {
 			}
 		}
 	}
+
 	/**
 	 * Show Upsell Message.
 	 *
@@ -160,17 +182,21 @@ class Pekky_WC_Stand_Alone_Force_Sells_Sorting {
 		if ( ! is_cart() ) {
 			return;
 		}
+
 		$cart       = WC()->cart;
 		$cart_total = WC()->cart->get_displayed_subtotal();
+
 		// Calculate Appropriately.
 		if ( $cart->display_prices_including_tax() ) {
 			$cart_total = round( $cart_total - ( $cart->get_discount_total() + $cart->get_discount_tax() ), wc_get_price_decimals() );
 		} else {
 			$cart_total = round( $cart_total - $cart->get_discount_total(), wc_get_price_decimals() );
 		}
+
 		// Check if cart total meets some qualifications.
 		$upsell_data      = array();
 		$last_large_price = 0; // Incase cart total is larger than 1 min_price in the list.
+
 		foreach ( self::$wc_suggested_upsell_products_data as $data ) {
 			// Does cart_total_qualify at all.
 			if ( $cart_total >= $data['min_price'] ) {
@@ -182,28 +208,45 @@ class Pekky_WC_Stand_Alone_Force_Sells_Sorting {
 				}
 			}
 		}
+
 		if ( ! empty( $upsell_data ) ) {
+
 			$p_id = $upsell_data['product_id'];
+
 			// Check if product is in cart already.
 			$cart_id = $cart->generate_cart_id( $p_id );
+
 			if ( $cart->find_product_in_cart( $cart_id ) ) {
 				// Product already in cart, no need to add notice.
 				return;
 			}
+
 			$product = wc_get_product( $p_id );
+
 			// Does product exist? to play safe and not break the site.
 			if ( ! $product ) {
 				return;
 			}
+			// Default link text if the upsell 'cart_btn_txt' is empty.
+			$default_link_txt = $product->get_name();
+
+			$link_text = ( isset( $upsell_data['cart_btn_txt'] ) && ! empty( $upsell_data['cart_btn_txt'] )
+			? $upsell_data['cart_btn_txt'] : $default_link_txt );
+
 			// Get link.
 			$raw_link = add_query_arg( 'add-to-cart', $p_id );
-			$link     = '<a href="' . esc_url( $raw_link ) . '" class="button"> Click Here to Upgrade </a>';
+			$link     = '<a href="' . esc_url( $raw_link ) . '" class="button">' . $link_text . '</a>';
+
 			// Translators: 1: The upsell note. 2:add to cart link.
 			$message = sprintf( __( '%1$s %2$s', 'woocommerce' ), $upsell_data['upsell_note'], $link );
+
 			wc_add_notice( $message );
+
 		}
 	}
+
 	// Inner functions to help run some parole.
+
 	/**
 	 * Sorts the stand alone products.
 	 *
@@ -236,14 +279,17 @@ class Pekky_WC_Stand_Alone_Force_Sells_Sorting {
 		}
 		// Get list of standalone product id.
 		$product_force_sells_list = $force_sells_products_map[ $product_id ];
+
 		if ( empty( $product_force_sells_list ) ) {
 			// Wrong list.
 			return false;
 		}
+
 		// Also add the force_sells product to cart/remove.
 		foreach ( $product_force_sells_list as $_id ) {
 			$p_id = ( $add_to_cart ? $_id : $wc_cart->generate_cart_id( $_id ) );
 			$wc_cart->{"{$cart_func}"}( $p_id );
+
 			// Reset quantity to 1.
 			if ( $add_to_cart ) {
 				$cart_item_key = $wc_cart->generate_cart_id( $p_id );
@@ -252,6 +298,7 @@ class Pekky_WC_Stand_Alone_Force_Sells_Sorting {
 		}
 		return true;
 	}
+
 }
 // Launch!
 Pekky_WC_Stand_Alone_Force_Sells_Sorting::init();
